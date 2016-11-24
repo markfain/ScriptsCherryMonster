c9d367f0705848f3b012daa4c116b9cc7a29d6ef
Mark Fainstein <mark@slatescience.com>
2016-11-24 10:36:21 +0200

    typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,6 @@
+/// <reference path="globals/chalk/index.d.ts" />
+/// <reference path="globals/commander/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/handlebars/index.d.ts" />
+/// <reference path="globals/progress/index.d.ts" />
+/// <reference path="globals/superagent/index.d.ts" />
