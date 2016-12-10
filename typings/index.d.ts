cf0216d8b9f2a79ec53e476592a350e9e88347ab
Mark Fainstein <mark@slatescience.com>
2016-12-10 22:49:51 +0200

    remove unneeded typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,1 @@
-/// <reference path="globals/chalk/index.d.ts" />
-/// <reference path="globals/commander/index.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/handlebars/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/progress/index.d.ts" />
-/// <reference path="globals/superagent/index.d.ts" />
+
